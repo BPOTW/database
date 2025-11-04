86f84788ad82d49d501049d2b16d0b4ddf39e343
Muhammad Zain Ali <133132057+BPOTW@users.noreply.github.com>
2025-11-04 10:38:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./utils/database.js";
+import router from "./routes/link.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+connectDB();
+
+app.use('/', router);
+
+
+app.listen('5050',()=>{
+    console.log('Server started successfully: https://localhost:5050')
+})
